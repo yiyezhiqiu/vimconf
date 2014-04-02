de706f3c6312345105f9fd6479e9ca1aeb4ccb26
Chenguang Zhang <chenguang.zhang@hulu.com>
2014-04-02 12:51:08 +0800

    Add instructions to install nodejs

--- install_libs.sh
+++ install_libs.sh
@@ -7,6 +7,13 @@
 pip install --upgrade pyflakes
 pip install --upgrade autopep8
 
-# Please install npm 
+# For MacOS
+# brew install node
+
+# For Ubuntu
+# apt-get install python-software-properties
+# apt-add-repository ppa:chris-lea/node.js
+# apt-get update
+# apt-get install nodejs
 
 npm install -g jshint
